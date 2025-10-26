2b106e2742bfae04ba10af7f4eb68db3e08ebeb9
Wu1v2z <artyr.comfy3065@gmail.com>
2025-10-26 21:57:59 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,7 +2,18 @@
 //  при цьому пропускаючи всі цифри.
 
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let result = '';
+
+  // Проходимо по рядку з кінця до початку
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str[i];
+    // Якщо символ НЕ є цифрою,а нормальним тіпом, додаємо його до результату
+    if (!(char >= '0' && char <= '9')) {
+      result += char;
+    }
+  }
+
+  return result;
 }
 
 console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
